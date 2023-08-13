78df5b7b62704d5e1eb1f0c8aecac74ecba1b2a6
ruhul <ra.01403288711@gmail.com>
2023-08-13 10:16:49 +0600

    create threading with function parameter

--- ProjectRepo/thread.py
+++ ProjectRepo/thread.py
@@ -1,1 +1,12 @@
-print("HKellopw")
+
+#step1- import thread class
+from threading import Thread
+#create a function containing code to be executed parallay
+def display(n,message):
+    for i in range(n):
+        print(message , n)
+#create new thread here
+t1 = Thread(target=display,args=(4,"Hi Ruhul Amin"))
+
+# start the new thread
+t1.start()
